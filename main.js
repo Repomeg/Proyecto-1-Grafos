5f52226a0176e81ffd727f0b178a32a7188f4f4c
Barry <repomeg@gmail.com>
2021-04-27 23:29:19 -0400

    primera versión

---
+++ main.js
@@ -0,0 +1,152 @@
+var nodes, edges, network;
+
+// convenience method to stringify a JSON object
+function toJSON(obj) {
+  return JSON.stringify(obj, null, 4);
+}
+
+
+function addNode() {
+  try {
+    nodes.add({
+      id: document.getElementById("node-id").value,
+      label: document.getElementById("node-label").value,
+    });
+  } catch (err) {
+    alert(err);
+  }
+}
+
+function updateNode() {
+  try {
+    nodes.update({
+      id: document.getElementById("node-id").value,
+      label: document.getElementById("node-label").value,
+    });
+  } catch (err) {
+    alert(err);
+  }
+}
+function removeNode() {
+  try {
+    nodes.remove({ id: document.getElementById("node-id").value });
+  } catch (err) {
+    alert(err);
+  }
+}
+
+function addEdge() {
+  try {
+    edges.add({
+      id: document.getElementById("edge-id").value,
+      from: document.getElementById("edge-from").value,
+      to: document.getElementById("edge-to").value,
+    });
+  } catch (err) {
+    alert(err);
+  }
+}
+function updateEdge() {
+  try {
+    edges.update({
+      id: document.getElementById("edge-id").value,
+      from: document.getElementById("edge-from").value,
+      to: document.getElementById("edge-to").value,
+    });
+  } catch (err) {
+    alert(err);
+  }
+}
+function removeEdge() {
+  try {
+    edges.remove({ id: document.getElementById("edge-id").value });
+  } catch (err) {
+    alert(err);
+  }
+}
+
+function dirigirNodos(){
+  if(document.getElementById("mynetwork") == true){
+    return true;
+  }
+  return false;
+}
+
+
+
+function draw() {
+  // create an array with nodes
+  nodes = new vis.DataSet();
+  nodes.on("*", function () {
+    document.getElementById("nodes").innerText = JSON.stringify(
+      nodes.get(),
+      null,
+      4
+    );
+  });
+  nodes.add([
+    { id: "1", label: "Node 1" },
+    { id: "2", label: "Node 2" },
+    { id: "3", label: "Node 3" },
+    { id: "4", label: "Node 4" },
+    { id: "5", label: "Node 5" },
+  ]);
+
+  // create an array with edges
+  edges = new vis.DataSet();
+  edges.on("*", function () {
+    document.getElementById("edges").innerText = JSON.stringify(
+      edges.get(),
+      null,
+      4
+    );
+  });
+  edges.add([
+    { id: "1", from: "1", to: "2" },
+    { id: "2", from: "1", to: "3" },
+    { id: "3", from: "2", to: "4" },
+    { id: "4", from: "2", to: "5" },
+  ]);
+
+  // create a network
+  var container = document.getElementById("mynetwork");
+  var data = {
+    nodes: nodes,
+    edges: edges,
+  };
+  var options = {
+    edges:{
+      arrows: {
+        to: {
+          enabled: true,//dirigirNodo(),
+          imageHeight: undefined,
+          imageWidth: undefined,
+          scaleFactor: 1,
+          src: undefined,
+          type: "arrow"
+        },
+        middle: {
+          enabled: false,
+          imageHeight: 32,
+          imageWidth: 32,
+          scaleFactor: 1,
+          src: "https://visjs.org/images/visjs_logo.png",
+          type: "image"
+        },
+        from: {
+          enabled: false,
+          imageHeight: undefined,
+          imageWidth: undefined,
+          scaleFactor: 1,
+          src: undefined,
+          type: "arrow"
+        }
+      }
+    }
+  };
+  network = new vis.Network(container, data, options);
+}
+
+window.addEventListener("load", () => {
+  draw();
+});
