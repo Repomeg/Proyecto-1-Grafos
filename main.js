23000a5c32cd1eb728a71415fed91dc8cc18c768
Barry <repomeg@gmail.com>
2021-05-25 17:57:19 -0400

    Se agregan funciones faltantes, se elimina css, se cambia gran parte del código

--- main.js
+++ main.js
@@ -5,17 +5,15 @@ var network = null;
 var vertices = null;
 var aristas_from = null;
 var aristas_to = null;
-
+//var aristas = [];
 var peso = null;
+var auxlista= new Map();
 
 var mAdyacencia = null;
 var mCaminos = null;
+var mPeso = null;
 
 var infociclo;
-
-var i;
-var j;
-var k;
 
 var form = document.querySelector("#form1");
 
@@ -31,7 +29,7 @@ function destroy() {
 }
 
 function buscarConexo(columna,fila){
-  for( i=0; i<(aristas_from.length);i++){
+  for(let i=0; i<(aristas_from.length);i++){
     if(form.Direccion.value=="Dirigido"){
       if(columna===aristas_from[i] && fila===aristas_to[i])
           return 1;
@@ -52,8 +50,8 @@ function llenarMatriz() {
   }
   mAdyacencia = [];
   var aux = []; // columnas
-    for( i=0; i<vertices.length;i++){
-      for( j=0; j<vertices.length;j++){
+    for(let i=0; i<vertices.length;i++){
+      for(let j=0; j<vertices.length;j++){
         if(buscarConexo(vertices[i],vertices[j])===1){
           aux.push(1);
          }
@@ -65,32 +63,147 @@ function llenarMatriz() {
       aux=[];
     }
 
-    for(var i=0; i<mAdyacencia.length; i++) {
+    for( i=0; i<mAdyacencia.length; i++) {
       var newRow = table.insertRow(table.length);
-      for(var j=0; j<mAdyacencia[i].length; j++) {
+      for( j=0; j<mAdyacencia[i].length; j++) {
         var cell = newRow.insertCell(j);
-
         cell.innerHTML = mAdyacencia[i][j];
       }
     }
-    MatCami();
-    conexo();
-    if(conexo()==true){
+
+    matrizCamino(mAdyacencia);
+    conexo(mCaminos);
+    if(conexo(mCaminos)==true){
       infociclo = ciclo();
       euleriano();
       if(infociclo==false){
         console.log(hamiltoniano());
       }
     }
-    //console.log(euler());
+    else {
+      euleriano();
+      hamiltoniano();
+    }
+
+    MatrizDePeso();
+    kruskal();
+    /*
+    console.log(auxlista);
+        shortestPath();
+    //console.log("El flujo máximo posible es" + fordFulkerson (mPeso ,0 ,5)); */
     return mAdyacencia;
+}
+
+
+function bfs(rGraph, s, t, parent) {
+	var visited = [];
+	var queue = [];
+	var V = rGraph.length;
+	// Create a visited array and mark all vertices as not visited
+	for (var i = 0; i < V; i++) {
+		visited[i] = false;
+	}
+	// Create a queue, enqueue source vertex and mark source vertex as visited
+	queue.push(s);
+	visited[s] = true;
+	parent[s] = -1;
+
+	while (queue.length != 0) {
+		var u = queue.shift();
+		for (var v = 0; v < V; v++) {
+			if (visited[v] == false && rGraph[u][v] > 0) {
+				queue.push(v);
+				parent[v] = u;
+				visited[v] = true;
+			}
+		}
+	}
+	//If we reached sink in BFS starting from source, then return true, else false
+	return (visited[t] == true);
+}
+
+function fordFulkerson(graph, s, t) {
+  if (s < 0 || t < 0 || s > graph.length-1 || t > graph.length-1){
+    throw new Error("Ford-Fulkerson-Maximum-Flow :: invalid sink or source");
+  }
+  if(graph.length === 0){
+    throw new Error("Ford-Fulkerson-Maximum-Flow :: invalid graph");
+  }
+	var rGraph = [];
+	for (var u = 0; u < graph.length; u++) {
+		var temp = [];
+    if(graph[u].length !== graph.length){
+      throw new Error("Ford-Fulkerson-Maximum-Flow :: invalid graph. graph needs to be NxN");
+    }
+		for (v = 0; v < graph.length; v++) {
+			temp.push(graph[u][v]);
+		}
+		rGraph.push(temp);
+	}
+	var parent = [];
+	var maxFlow = 0;
+
+	while (bfs(rGraph, s, t, parent)) {
+		var pathFlow = Number.MAX_VALUE;
+		for (var v = t; v != s; v = parent[v]) {
+			u = parent[v];
+			pathFlow = Math.min(pathFlow, rGraph[u][v]);
+		}
+		for (v = t; v != s; v = parent[v]) {
+			u = parent[v];
+			rGraph[u][v] -= pathFlow;
+			rGraph[v][u] += pathFlow;
+		}
+
+
+		maxFlow += pathFlow;
+	}
+	// Return the overall flow
+  console.log(maxFlow);
+	return maxFlow;
+}
+
+function LlamarFord(){
+  var inicio= document.getElementById("inicio").value;
+  var final= document.getElementById("final").value;
+  fordFulkerson(mPeso,inicio,final);
+}
+
+function buscarPeso(columna,fila){
+  for(let i=0; i<aristas_from.length ;i++){
+    if(columna===aristas_from[i] && fila===aristas_to[i]){
+      return peso[i];
+    }
+    else{
+      if(columna===aristas_from[i] && fila===aristas_to[i] || columna===aristas_to[i] &&  fila===aristas_from[i])
+        return peso[i];
+    }
+  }
+}
+
+function MatrizDePeso(){ 
+  mPeso = [];
+  var aux = [];
+  for(let i=0; i<vertices.length;i++){
+    for(let j=0; j<vertices.length;j++){
+      if(buscarConexo(vertices[i],vertices[j])===1){
+        aux.push(buscarPeso(vertices[i],vertices[j]));
+      }
+      else{
+        aux.push(0);
+      }    
+    }
+    mPeso[i]=aux;
+    aux=[];
+  }
+  console.log(mPeso);
+  return mPeso;
 }
 
 function conexo(){
   var  cont=0;
-  
-  for( i=0; i<vertices.length;i++){
-    for( j=0; j<vertices.length;j++){
+  for(let i=0; i<vertices.length;i++){
+    for(let j=0; j<vertices.length;j++){
       if(mCaminos[i][j]===0){
         cont++;
       }
@@ -108,15 +221,13 @@ function conexo(){
   }
 }
 
-function MatCami(){
-  var a;
-  var b;
+function matrizCamino(){
   var table = document.getElementById("TablaCam");
-  table.innerHTML = "";
+  var largo = mAdyacencia.length;
   mCaminos= mAdyacencia;
-  for( k = 0; k < mCaminos.length; k++){
-    for( i = 0; i < mCaminos.length; i++){
-      for( j = 0; j < mCaminos.length; j++){
+  for(let k = 0; k < largo; k++){
+    for(let i = 0; i < largo; i++){
+      for(let j = 0; j < largo; j++){
         if(mAdyacencia[i][j] == 1 || mAdyacencia[i][k] == 1 && mAdyacencia[k][j] == 1){
           mAdyacencia[i][j] = 1;
         }
@@ -134,6 +245,7 @@ function MatCami(){
       cell.innerHTML = mCaminos[a][b];
     }
   }
+  mCaminos = mAdyacencia;
   console.log(mCaminos);
   return mCaminos;
 }
@@ -142,23 +254,19 @@ function gradoVertice(x){
   var aux = 0;
   var guardaNodo1 = 0;
   var guardaNodo2 = 0;
-  
-  for( i = 0; i<vertices.length; i++){
-    
+  for(let i = 0; i<vertices.length; i++){
     guardaNodo1 = aristas_from[i];
     guardaNodo2 = aristas_to[i];
-    
     if(x === guardaNodo1 || x === guardaNodo2){
       aux++;
     }
   }
-  console.log(aux);
   return aux;
 }
 
 function ciclo(){
   var grado, cont = 0;
-  for( i =0;i<vertices.length;i++){
+  for(let i =0;i<vertices.length;i++){
     grado = gradoVertice(vertices[i]);
     if(grado == 2){
       cont++;
@@ -166,78 +274,158 @@ function ciclo(){
   }
   if(vertices.length==cont){
     console.log("Es hamiltoniano");
+    document.getElementById('hamiltoniano').innerHTML='Su grafo es hamiltoniano';
     return true;
   }
   else return false;
 }
 
 function hamiltoniano(){
-  var cont = 0;
+  cont = 0;
   if(conexo() === true){
       if(vertices.length>=3){
-        for( i = 0; i<vertices.length; i++){
-          
+        for(let i = 0; i<vertices.length; i++){
+          console.log(vertices[i]);
+          console.log(gradoVertice(vertices));
           if(gradoVertice(vertices[i]) >= (vertices.length/2)){
             cont++;
           }
-          
+          console.log(cont);
         }
         if(cont === vertices.length){
           console.log("Es hamiltoniana");
-          return true;
+          document.getElementById('hamiltoniano').innerHTML='Su grafo es hamiltoniano';
         }
         else{
           console.log("No es hamiltoniana");
-          return false;
+          document.getElementById('hamiltoniano').innerHTML='Su grafo NO es hamiltoniano';
         }
       }
       else{
         console.log("No es hamiltoniana");
-        return false;
+        document.getElementById('hamiltoniano').innerHTML='Su grafo NO es hamiltoniano';
       }
     }
     else{
     console.log("No es hamiltoniana");
-    return false;
+    document.getElementById('hamiltoniano').innerHTML='Su grafo NO es hamiltoniano';
   }
 }
 
 function euleriano(){
   var contPar = 0;
   if(conexo() === true){
-    for( i = 0; i<vertices.length; i++){
+    for(let i = 0; i<vertices.length; i++){
       if(gradoVertice(vertices[i])%2 === 0){
         contPar++;
       }
     }
     if(contPar === vertices.length){
       console.log("Es euleriano");
+      document.getElementById('euleriano').innerHTML='Su grafo es euleriano';
     }
     else{
       console.log("No es euleriano");
+      document.getElementById('euleriano').innerHTML='Su grafo NO es euleriano';
     }
   }
   else{
     console.log("No es euleriano");
+    document.getElementById('euleriano').innerHTML='Su grafo NO es euleriano';
   }
 }
 
-/*
-function euler(){
-  resultadoEuler = document.getElementById("resultadoEuler");
-  var grado = [];
-  grado = gradoGrafo(vertices,aristas);
-  // Verifica que no tenga aristas dirigidas
-  for(i=0; i<aristas.length; i++){
-      if(aristas[i][2] == true){
-          console.error("El grafo no es Euleriano");
-          alert("No es euleriano");
-          //resultadoEuler.innerHTML = false
-          return 0;
-      }
+function agregarConex(nodoInicial, nodoFinal, valorDistancia) {
+  var arrayaux;
+  valorDistancia = parseInt(valorDistancia, 10);
+
+  buscarNodo = grafoDijkstra.filter(item => item.origen === nodoInicial);
+  if (buscarNodo.length === 0) {
+    conexion = [];
+    conexion.push({
+      destino: nodoFinal,
+      distancia: valorDistancia
+    });
+    grafoDijkstra.push({ origen: nodoInicial, conexiones: conexion });
+  }
+  else {
+    buscarNodo[0].conexiones.push({
+      destino: nodoFinal,
+      distancia: valorDistancia
+    });
+  };
+
+};
+
+function shortestPath(){
+  grafoDijkstra = [];
+  var enlaces;
+  var desde= document.getElementById("desde").value;
+  var hasta= document.getElementById("hasta").value;
+
+  for(i = 0; i < aristas_from.length; i++){
+    agregarConex(aristas_from[i], aristas_to[i], peso[i]);
+    agregarConex(aristas_to[i], aristas_from[i], peso[i]);
+  }
+
+  var g = new Graph();
+
+  grafoDijkstra.forEach(function (value, key, array) {
+    enlaces = {};
+
+    value.conexiones.forEach(function (conexion, key, array) {
+      enlaces[conexion.destino] = conexion.distancia;
+      
+    });
+   
+    g.addVertex(value.origen, enlaces);
+
+    short = g.shortestPath(desde, hasta).concat(desde).reverse();
+  });
+
+  if(short.length>1){ //Realiza la comprobacion en 1 por que el nodo de inicio ya esta dentro del arreglo
+    document.getElementById("Camino").innerHTML='El camino mas corto desde '+ desde + ' hasta ' + hasta + ' es: '+ short; 
+  }
+  else{
+    document.getElementById("Camino").innerHTML='No existe un camino valido para esa ruta';
   }
 }
-*/
+
+function kruskal(){
+  var nodoA = [];
+  var nodoB = [];
+  var arcos = [];
+
+  var mKruskal = mPeso;
+  var min = 9999;
+  var ejeX = 0;
+  var ejeY = 0;
+
+  for(var i = 0; i < vertices.length; i++){
+    for(var j = 0; j < vertices.length; j++){
+      if(min > mKruskal[ejeX][j] && mKruskal[ejeX][j] != 0){
+        min = mKruskal[ejeX][j];
+        ejeY = j;
+      }
+      else{
+        if(mKruskal[j][ejeX] < min && mKruskal[j][ejeX] != 0){
+          min = mKruskal[j][ejeX];
+          ejeY = j;
+        }
+      }
+    }
+    if(min != 0 || min != 9999){
+      nodoA.push(ejeX);
+      nodoB.push(ejeY);
+      arcos.push(min);
+      mKruskal[ejeX][ejeY] = 0;
+      mKruskal[ejeY][ejeX] = 0;
+      ejeX = ejeY;
+      min = 9999;
+    }
+  }
+  console.log(nodoA, nodoB, arcos);
+}
 
 function draw() {
   destroy();
@@ -262,21 +450,21 @@ function draw() {
         document.getElementById("node-operation").innerText = "Add Node";
         editNode(data, clearNodePopUp, callback);
       },
-      editNode: function (data, callback) {
+     /* editNode: function (data, callback) {
         // filling in the popup DOM elements
         document.getElementById("node-operation").innerText = "Edit Node";
         editNode(data, cancelNodeEdit, callback);
-      },
+      },*/
       addEdge: function (data, callback) {
         if (data.from == data.to) {
-          var r = confirm("Desea conectar el nodo a si mismo?");
+          var r = confirm("Do you want to connect the node to itself?");
           if (r != true) {
             callback(null);
             return;
           }
         }
         if(form.Direccion.value=="Dirigido"){
-          var options1 = {
+          var options = {
             edges:{
               arrows:{
                 to:{
@@ -287,10 +475,10 @@ function draw() {
               }     
             }
           }
-          network.setOptions(options1);
+          network.setOptions(options);
         }
         else {
-          var options2 = {
+          var options = {
             edges:{
               arrows:{
                 to:{
@@ -301,12 +489,14 @@ function draw() {
               }
             }
           }
-          network.setOptions(options2);
+          network.setOptions(options);
         }
         
         document.getElementById("edge-operation").innerText = "Add Edge";
         editEdgeWithoutDrag(data, callback);
       },
+      deleteNode: false,
+      deleteEdge: false,
       editEdge: {
         editWithoutDrag: function (data, callback) {
           document.getElementById("edge-operation").innerText = "Edit Edge";
@@ -320,13 +510,12 @@ function draw() {
 }
 
 function editNode(data, cancelAction, callback) {
-  document.getElementById("node-label").value = data.label;
+  
   document.getElementById("node-saveButton").onclick = saveNodeData.bind(this, data, callback);
   document.getElementById("node-cancelButton").onclick = cancelAction.bind(this, callback);
   document.getElementById("node-popUp").style.display = "block";
 }
 
-// Callback passed as parameter is ignored
 function clearNodePopUp() {
   document.getElementById("node-saveButton").onclick = null;
   document.getElementById("node-cancelButton").onclick = null;
@@ -340,8 +529,8 @@ function cancelNodeEdit(callback) {
 
 function saveNodeData(data, callback) {
   data.id = document.getElementById("node-id").value;
-  data.label = document.getElementById("node-label").value;
-  for( i=0; i < vertices.length ;i++){
+  data.label = document.getElementById("node-id").value;
+  for(var i=0; i < vertices.length ;i++){
     if(vertices[i]==data.id){
       clearNodePopUp();
       return alert("Nodo ya existente");  
@@ -379,13 +568,12 @@ function saveEdgeData(data, callback) {
     data.from = data.from.id;
 
   data.label = document.getElementById("edge-label").value;
-
-  clearEdgePopUp();
-  callback(data);
-  
+  //aristas[aristas_from.length] = [data.from, data.to];
   aristas_from.push(data.from);
   aristas_to.push(data.to);
   peso.push(data.label);
+  clearEdgePopUp();
+  callback(data);
 }
 
 function init() {
